4e93186010de2f866baac905b82aba2e79050eee
ceryssc <cs.clarke90@gmail.com>
2019-03-29 09:45:43 +1300

    changed router names

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ server.engine('hbs', hbs({
 server.set('view engine', 'hbs')
 server.use(express.static('public'))
 server.use(express.urlencoded({ extended: false }))
-server.use('/', puppyRoutes)
+server.use('/', planetRoutes)
 
 
 module.exports = server
