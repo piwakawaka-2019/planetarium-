34881eb7a720e0b99f66dac90dc3cd50fb956c02
ceryssc <cs.clarke90@gmail.com>
2019-03-29 10:18:05 +1300

    changes made

--- routes.js
+++ routes.js
@@ -3,9 +3,25 @@ const router = express.Router()
 const fs =require ('fs')
 
 router.get('/', (req, res) => {
-  res.send('planets!!!!')
-  
+  // res.send('planets!!!!')
+  res.redirect('/home')
 })
+
+//getting to home page 
+
+router.get('/home', (req,res) => {
+
+
+  fs.readFile('planetinfo.json', 'utf8', (err, data) => {
+
+    let planetinfo = JSON.parse(data)
+
+  res.render('/planets/index', planetData)
+  })
+})
+
+
+
 
 
 module.exports = router
