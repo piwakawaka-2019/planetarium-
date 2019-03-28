582125ed8392009b1f514fdf8e3db43d40419b42
ceryssc <cs.clarke90@gmail.com>
2019-03-29 09:33:33 +1300

    enviro set up

--- server.js
+++ server.js
@@ -5,4 +5,16 @@ const server = express()
 const planetRoutes = require('./routes')
 
 server.use('/', planetRoutes)
+
+server.engine('hbs', hbs({
+  defaultLayout: 'main',
+  extname: 'hbs'
+}))
+server.set('view engine', 'hbs')
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+server.use('/', puppyRoutes)
+
+
 module.exports = server
+
