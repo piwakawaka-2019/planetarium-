defd661a1ace3f6badbc6407c2327a14caa9eccf
zozo-url <zozo-url@protonmail.ch>
2019-03-29 13:58:58 +1300

    add routes

--- routes.js
+++ routes.js
@@ -2,10 +2,31 @@ const express = require('express')
 const router = express.Router()
 const fs =require ('fs')
 
-router.get('/', (req, res) => {
-  res.send('planets!!!!')
+const planetinfo = require('./data/planetinfo.json')
+const randomfacts = require('./data/randomfacts.json')
+
+// router.get('/', (req, res) => {
+//   res.send('planets!!!!')
   
+// })
+
+
+router.get('/', (req, res)=>{
+
+  res.render("./planets/index.hbs", planetinfo)
 })
+
+
+
+
+// router.get('/', (req, res)=>{
+ 
+//     res.render("./planets/index.hbs", randomfacts)
+
+// })
+
+
+
 
 
 module.exports = router
