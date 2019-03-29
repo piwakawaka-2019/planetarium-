0d3f68a3df9ea38d242f356398ca3381d42ade93
ceryssc <cs.clarke90@gmail.com>
2019-03-29 15:12:35 +1300

    form sorted

--- routes.js
+++ routes.js
@@ -43,16 +43,51 @@ router.get('/planets/edit/:planet', (req, res) => {
 
   let Id = req.params.planet
 
-fs.readFile('./data/planetinfo.json', 'utf8', (err, data) => {
-  if (err) console.log(err)
-  console.log(data)
-  let planetDetails = JSON.parse(data)
-  console.log(planetDetails)
+  fs.readFile('./data/planetinfo.json', 'utf8', (err, data) => {
+    if (err) console.log(err)
+    console.log(data)
+    let planetDetails = JSON.parse(data)
+    console.log(planetDetails)
 
-  let single = planetDetails.planets.find((item) => item.planet == Id)
-  console.log(single)
-  res.render('planets/edit', single)
+    let single = planetDetails.planets.find((item) => item.planet == Id)
+    console.log(single)
+    res.render('planets/edit', single)
+  })
 })
+
+
+router.post('/planets/edit/:planet', (req, res) => {
+
+  let Id = req.params.planet
+
+  let newData = req.body
+  console.log("body", newData)
+
+
+
+  fs.readFile('./data/planetinfo.json', 'utf8', (err, data) => {
+    if (err) console.log(err)
+
+
+    let planetDetails = JSON.parse(data)
+    let single = planetDetails.planets.find((item) => item.planet == Id)
+
+    single.planet = newData.planet
+    single.moons = newData.moons
+    single.diameter = newData.diameter
+    single.distanceFromSun = newData.distanceFromSun
+
+    fs.writeFile('./data/planetinfo.json', JSON.stringify(planetDetails), (err) => {
+      if (err) {
+        res.send("Error Happened")
+        return
+      }
+      res.redirect("/planets/" + Id)
+      let planetDetails = JSON.parse(data)
+    })
+
+  })
+
 })
 
 module.exports = router
